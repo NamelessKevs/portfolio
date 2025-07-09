9b862cef44088734d7855b6db7f4a5b9c1e94132
NamelessKevs <149979072+NamelessKevs@users.noreply.github.com>
2025-07-09 11:30:07 +0800

    update

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 const loadingPairs = [
     { text: "Preparing something awesome...", image: "img/loading-scream.png" },
     { text: "Hang tight! It's almost ready...", image: "img/loading-scream.png" },
-    { text: "Building the magic for you...", image: "img/loading-scream.png" },
+    { text: "Building the magic for you...", image: "img/galaxy.png" },
     { text: "Just a moment, getting things ready...", image: "img/loading-scream.png" },
     { text: "Almost there, hold on tight!", image: "img/loading-scream.png" },
     { text: "Planning, Designing, Coding, DEVELOPING!", image: "img/loading-scream.png" },
